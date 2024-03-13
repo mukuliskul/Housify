e61d80efc8739af2050cf5dbc47f5f9e4d25449a
Jashanpreet Singh <jashan20preet21@gmail.com>
2024-03-13 14:29:07 -0400

    add addproperty page

--- client/pages/manageproperty/index.js
+++ client/pages/manageproperty/index.js
@@ -1,63 +1,24 @@
-import { useState } from "react";
-export default function AddProperty() {
-  async function handleSubmit(e) {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    fetch("/api/addPropertyApi", { method: "POST", body: formData });
-  }
+import Navbar from "@/components/navbar.js";
+import Property from "@/components/property.js";
+import AddProperty from "@/components/addProperty.js";
+export default function ManageProperty() {
+  let houses = ["99 xyz avenue, North York, ON M2H 2K1", "999 xyz street, North York, ON M2H 2K1"];
   return (
-    <form
-      method="post"
-      onSubmit={handleSubmit}
-      encType="multipart/form-data"
-      className="text-black w-full h-[600px] ml-[30px] mt-[30px] flex flex-col justify-around items-center"
-    >
-      <label>
-        Postal Code:
-        <input
-          type="text"
-          name="postal-code"
-          id="postal-code"
-          placeholder="Postal Code"
-          pattern="^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$"
-          class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
-          required
-        />
-      </label>
-
-      <label
-        className="w-[400px] block mb-2 text-sm font-medium text-gray-900"
-        htmlFor="title-certificate"
-      >
-        Title Certificate
-        <input
-          name="title-certificate"
-          type="file"
-          className="w-full text-black text-lg bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-primary file:hover:bg-[#062f9e] file:text-white rounded"
-          required
-        />
-      </label>
-      <label
-        className="block mb-2 text-sm font-medium text-gray-900"
-        htmlFor="property-tax-receipts"
-      >
-        Property Tax Receipts
-        <input
-          name="property-tax-receipts"
-          type="file"
-          className="w-full text-black text-lg bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-primary file:hover:bg-[#062f9e] file:text-white rounded"
-          multiple
-        />
-      </label>
-      <button
-        className="w-[215px] bg-primary h-[51px] rounded-2xl"
-        type="submit"
-      >
-        <p className="py-[5px] text-[20px] font-[500] text-white text-center">
-          Submit
+    <>
+      <div className="w-[100%] gap-y-[40px] h-[100vh] text-black kumbh-sans-font">
+        <p className="sm:text-left text-center w-[90%] sm:ml-[5px] sm:w-auto mx-auto my-[10px] text-primary font-[600] text-[35px]">
+          Manage Property
         </p>
-      </button>
-    </form>
+        <div className="lg:w-[80%] mx-auto flex flex-col sm:flex-row justify-around items-center sm:flex-wrap">
+          {houses?.map((house) => {
+            return (
+              <Property address={house} key={house} className="m-[50px]"/>
+            )
+            })
+          }
+          <AddProperty className="m-[50px]"/>
+        </div>
+      </div>
+    </>
   );
 }
