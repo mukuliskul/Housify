c52e390f1ce0252aab031a823b3e352cf2a3026a
Prabhjot Singh Longia <137945912+LongiaPrabhjot@users.noreply.github.com>
2024-03-17 06:43:13 +0000

    Added form template for reference

--- client/pages/generate/index.js
+++ client/pages/generate/index.js
@@ -128,6 +128,29 @@ export default function Generate() {
 		}
 	}, []);
 
+	const [startDate, setStartDate] = useState("");
+	const [endDate, setEndDate] = useState("");
+
+	const handleStartDateChange = (e) => {
+		const newStartDate = e.target.value;
+		setStartDate(newStartDate);
+
+		if (endDate && newStartDate > endDate) {
+			setEndDate(newStartDate);
+		}
+	};
+
+	const handleEndDateChange = (e) => {
+		setEndDate(e.target.value);
+	};
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		const form = e.target;
+		const formData = new FormData(form);
+		fetch("/api/uploadTenant", { method: "POST", body: formData });
+	}
+	
 	return (
 		<div className="grid lg:grid-cols-2 grid-col-1 text-black kumbh-sans-font">
 			<input className="hidden" id="file-input" type="file" />
@@ -151,12 +174,86 @@ export default function Generate() {
 					</div>
 					<div className="flex lg:flex-row justify-left mt-5">
 						<div className="w-[35%] max-w-[250px] border border-black bg-white rounded-lg max-w-xs">
-							<Image
-								src={fileIcon}
-								alt="file icon"
-								className="mx-auto py-[50%]"
-							/>
-						</div>
+						<form
+					method="post"
+					onSubmit={handleSubmit}
+					encType="multipart/form-data"
+					className="text-black w-full space-y-[20px] p-2 flex flex-col justify-around items-start"
+				>
+					<label>
+						Landlord Full Name
+						<input
+							type="text"
+							name="landlord-name"
+							id="landlord-name"
+							placeholder="Name"
+							class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+							required
+						/>
+					</label>
+
+					<label>
+						Tenant Full Name
+						<input
+							type="text"
+							name="tenant-name"
+							id="tenant-name"
+							placeholder="Name"
+							class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+							required
+						/>
+					</label>
+
+					<label>
+						Lease Start Date
+						<input
+							type="date"
+							name="start-date"
+							id="start-date"
+							value={startDate}
+							onChange={handleStartDateChange}
+							class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+							required
+						/>
+					</label>
+
+					<label>
+						Lease End Date
+						<input
+							type="date"
+							name="end-date"
+							id="end-date"
+							value={endDate}
+							onChange={handleEndDateChange}
+							min={startDate}
+							class="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]"
+							required
+						/>
+					</label>
+
+					{/* <label
+						className="w-[400px] block mb-2 font-medium text-gray-900"
+						htmlFor="tenant-lease"
+					>
+						Tenant Lease
+						<input
+							name="tenant-lease"
+							type="file"
+							className="w-full text-black text-lg bg-gray-100 file:cursor-pointer cursor-pointer file:border-0 file:py-3 file:px-4 file:mr-4 file:bg-primary file:hover:bg-[#062f9e] file:text-white rounded"
+							required
+						/>
+					</label> */}
+
+					{/* <button
+						className="w-[215px] bg-primary h-[51px] rounded-2xl"
+						type="submit"
+					>
+						<p className="py-[5px] text-[20px] font-[500] text-white text-center">
+							Submit
+						</p>
+					</button> */}
+				</form>
+					</div>
 						<div className="flex flex-col max-w-[350px] justify-around m-5 space-y-5">
 							<button
 								type="button"
